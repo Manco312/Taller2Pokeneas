163f9a467423543f6574abed9ec5daf9eaee3b83
Manco <santiagomancomaya@gmail.com>
2025-11-02 17:39:27 -0500

    Cambiar puerto

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const pokeneaRoutes = require('./routes/pokeneaRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
